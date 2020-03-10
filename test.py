ea63f90b530f06b04721aa3a92dc9e868de82e49
naly-ra <62013609+naly-ra@users.noreply.github.com>
2020-03-10 12:03:46 +0100

    def pop

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
-import numpy
+import numpy as np
+
+def pop(i,t):
+    
+    M=[]
+    ep = np.arange(start=0,stop=i)
+    for e in range(0,t):
+        M.append(np.random.choice(ep))
+    return M    
